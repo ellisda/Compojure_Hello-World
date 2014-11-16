6b5b0496d6e2c90db0d45a288714803d2191aab4
David Ellis <ellisda@gmail.com>
2014-11-16 10:51:58 -0700

    add ring-json to project.clj and it still loads in lein

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.2.0"]
-                 [ring/ring-defaults "0.1.2"]]
+                 [ring/ring-defaults "0.1.2"]
+                 [ring/ring-core "1.2.0"]
+                 [ring/ring-json "0.2.0"]]
   :plugins [[lein-ring "0.8.13"]]
   :ring {:handler hello-world.core.handler/app}
   :profiles
