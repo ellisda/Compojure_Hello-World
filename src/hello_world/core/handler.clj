d5fb88e82c0b76c9d98f1a905863669d0bba5138
David Ellis <ellisda@gmail.com>
2014-11-16 11:08:53 -0700

    a new route that responds with some json
    required change to "app declaration by wrapping wrap-json-response middleware around the route handler"
    
    also needed the extra namespace declaration for [compojure.handler :as handler]
    ref: http://zaiste.net/2014/02/web_applications_in_clojure_all_the_way_with_compojure_and_om/

--- src/hello_world/core/handler.clj
+++ src/hello_world/core/handler.clj
@@ -1,13 +1,17 @@
 (ns hello-world.core.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [compojure.handler :as handler]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.util.response :refer [resource-response response]]
+            [ring.middleware.json :as middleware]))
 
 (defn parse-int [s]
    (Integer. (re-find  #"\d+" s )))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
+  (GET  "/widgets" [] (response [{:name "Widget 1"} {:name "Widget 2"}]))
   (GET "/clojure" [] str (re-seq #"\d+" "123 456 789"))
   (GET "/user/:id" [id] (str "<h1>Hello user " (+ 10 (parse-int id)) " (we added 10) </h1>"))
   (GET "/ip" {ip :remote-addr} (str "<h1>Hello client IP " ip "  </h1>"))
@@ -21,7 +25,12 @@
             (assoc-in response [:headers "Content-Type"] content-type))))
 
 (def app
-  (wrap-content-type app-routes "text/html"))
+  (-> (handler/api app-routes)
+      (middleware/wrap-json-body)
+      (middleware/wrap-json-response)))
+
+
+; Rev2  (wrap-content-type app-routes "text/html"))
 
 ;ORIG -  (wrap-defaults app-routes site-defaults))
   ;image/jpeg
