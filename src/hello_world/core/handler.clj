914df42cfaa1ff8c9d9b8edbafcc6a117e79c7a1
David Ellis <ellisda@gmail.com>
2014-11-16 19:25:04 -0700

    Added a 2nd clojure module, and configured a route to call into it
    module returns a sible map that is responsed to client as json

--- src/hello_world/core/handler.clj
+++ src/hello_world/core/handler.clj
@@ -1,4 +1,5 @@
 (ns hello-world.core.handler
+  (:use hello-world.core.data-stubs)
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [compojure.handler :as handler]
@@ -11,11 +12,13 @@
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
-  (GET  "/widgets" [] (response [{:name "Widget 1"} {:name "Widget 2"}]))
+  (GET  "/widgets" [] (response [{:title "Sweet Emotion", :id "x2342134lkjaslk3"} {:name "Widget 2"}]))
   (GET "/clojure" [] str (re-seq #"\d+" "123 456 789"))
   (GET "/user/:id" [id] (str "<h1>Hello user " (+ 10 (parse-int id)) " (we added 10) </h1>"))
   (GET "/ip" {ip :remote-addr} (str "<h1>Hello client IP " ip "  </h1>"))
   (GET "/requestmap" request (str request))
+  (GET "/index-page" [] (index-page))
+  (GET "/assoc-words" [] (response (reduce assoc-words {} (range 0 10))))
   (route/not-found "Not Found"))
 
 ; NEW CODE - attempting to play with Middleware functions
